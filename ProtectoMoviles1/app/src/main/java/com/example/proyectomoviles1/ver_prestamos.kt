8798a1b498aa2cc9bbba7bb9ff3a15690cea965f
jkincho23 <j.jgr230801@gmail.com>
2023-04-23 13:38:33 -0600

    Iniciando jajaja

---
+++ ProtectoMoviles1/app/src/main/java/com/example/proyectomoviles1/ver_prestamos.kt
@@ -0,0 +1,11 @@
+package com.example.proyectomoviles1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ver_prestamos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ver_prestamos)
+    }
+}
